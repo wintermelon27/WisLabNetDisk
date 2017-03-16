d920216d7b82bf8aa2e5cbec440fd862b09ecc32
wintermelon27 <450340090@qq.com>
2017-03-17 02:42:12 +0800

    【修改】增加登录与注册功能

--- NetDiskPro/app/auth/views.py
+++ NetDiskPro/app/auth/views.py
@@ -1,14 +1,15 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
@@ -21,3 +22,14 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can login now.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
--- NetDiskPro/app/models.py
+++ NetDiskPro/app/models.py
@@ -3,19 +3,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
-class Role(UserMixin, db.Model):
+class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
+    # email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
@@ -25,7 +26,7 @@ class User(db.Model):
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    users = db.relationship('User', backref='role')
+    # users = db.relationship('User', backref='role')
 
     password_hash = db.Column(db.String(128))
 
